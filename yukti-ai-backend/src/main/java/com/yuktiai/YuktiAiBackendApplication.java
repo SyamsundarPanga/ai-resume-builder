783577cf0672d13848916dc990e87c1710673b72
bluepal-syamsundar-panga <syamsundar.panga@bluepal.com>
2026-07-17 17:28:44 +0530

    Basic SetUp for whole application backend and frontend

---
+++ yukti-ai-backend/src/main/java/com/yuktiai/YuktiAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yuktiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuktiAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuktiAiBackendApplication.class, args);
+	}
+
+}
---
+++ yukti-ai-backend/src/test/java/com/yuktiai/YuktiAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuktiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuktiAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
